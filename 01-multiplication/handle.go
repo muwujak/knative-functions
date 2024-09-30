96eab6257348e2d856cccf094a69c959d3bbfc0c
mujak27 <mujakoverflow@gmail.com>
2024-09-30 23:38:45 +0700

    day 01: set response type, decode request body, error

--- 01-multiplication/handle.go
+++ 01-multiplication/handle.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"strconv"
+	"encoding/json"
 )
 
 // Handle an HTTP Request.
 func Handle(w http.ResponseWriter, r *http.Request) {
-	/*
-	 * YOUR CODE HERE
-	 *
-	 * Try running `go test`.  Add more test as you code in `handle_test.go`.
-	 */
+	
+	// set response to json
+	w.Header().Set("Content-Type", "application/json");
+
+	// parse request body consists of json
+	var reqData RequestData;
+	err := json.NewDecoder(r.Body).Decode($reqData);
+	if err != nil {
+		http.Error(w, "Invalid request body");
+		return;
+	}
+	
 
 	dump, err	 := httputil.DumpRequest(r, true)
 	if err != nil {
